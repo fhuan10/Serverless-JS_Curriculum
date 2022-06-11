a56a9ab0a83d8aeadba348143ae06741a26a8f0c
fhuan10 <99567644+fhuan10@users.noreply.github.com>
2022-06-11 01:53:35 -0500

    Enter

--- late.js
+++ late.js
@@ -1,1 +1,15 @@
 // start coding your function here!
+
+function running_late(date) {
+    var currentDate = new Date(date);
+    var hours = currentDate.getHours();
+
+
+    if (currentDate.getHours() >= 22) {
+        console.log('It is late!');
+    } else {
+        console.log('It is early!');
+    }
+}
+
+exports.running_late = running_late;
